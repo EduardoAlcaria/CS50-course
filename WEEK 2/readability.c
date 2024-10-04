6bb1bed3503dea1e13da3ecd711c8b1993695111
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-04 14:12:43 -0300

    challenge 3 week 2 unfinished (added sentences counter)

--- WEEK 2/readability.c
+++ WEEK 2/readability.c
@@ -5,8 +5,10 @@ int analize(str);
 
 int main(void)
 {
+    
     int words = 0, j;
     int letters = 0, i;
+    int sen = 0, h;
     char str[100];
     printf("text: ");
     scanf("%[^\n]s", str);
@@ -31,7 +33,18 @@ int analize(str);
         }
         
     }
+//number of sentences
+    for (int h = 0; str[h] != '\0'; h++)
+    {
+        if (str[h] == '.' || str[h+1] == '\0')
+        {
+            sen++;
+        }
+        
+    }
+    
     printf("Letters %d\n", letters);
     printf("Words %d\n", words + 1);
+    printf("Senteces %d\n", sen);
 }
 }

eaae456127c856d65a4a340dcaacd75d2fde4af5
EduardoAlcaria <eduardoalcarialopes1@outlook.com>
2024-10-11 16:55:25 -0300

    improvement of the names and numbers register with data struct

--- WEEK 3/phonenumber.c
+++ WEEK 3/phonenumber.c
@@ -1,21 +1,36 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef struct
+{
+    char name[50];
+    char number[20];
+}
+person;
+
 int main(void)
 {
     char name[50];
-    char *names[] = {"carter", "david", "john"};
-    char number[50];
-    char *numbers[] = {"51-991232145", "51-991232145", "51-9912301231"};
-
     printf("Search: ");
     scanf("%49s", name);
 
+    person people[3];
+    strcpy(people[0].name,"john");
+    strcpy(people[0].number, "51-9923102031");
+
+    strcpy(people[1].name, "david");
+    strcpy(people[1].number, "51-9923102031");
+
+    strcpy(people[2].name, "carter");
+    strcpy(people[2].number, "51- 9912412510");
+    
+
+
     for (int i = 0; i < 3; i++)
     {
-        if (strcmp(names[i], name) == 0)
+        if (strcmp(people[i].name, name) == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
          
